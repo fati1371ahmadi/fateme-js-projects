cee7e78b06f47bf44c049896f90f410ff4e81acf
Fatemeh Ahmadi <fati1371ahmadi.rayan@gmail.com>
2023-07-26 22:46:16 +0330

    feat/fati1371ahmadi/first commit and added js file and html file

---
+++ js-01/index.js
@@ -0,0 +1,7 @@
+let userDOB = +prompt ("Please Enter Your Birthday Year");
+let userAge = +prompt ("Please Enter Your Age");
+
+let userBirthdayYear = userDOB + userAge;
+
+
+document.write("This Year is",userBirthdayYear)
